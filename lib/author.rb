93fb9bc72e7c5dc279d26c313e1e980330def005
Nathan Steed <steedn@outlook.com>
2020-08-11 20:13:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,15 +14,6 @@ class Author
     @@all
   end
 
-  def add_post(post)
-    post.author = self
-  end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
-    post.author = self
-  end
-
   def posts
     Post.all.select {|post| post.author == self}
   end
