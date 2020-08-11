61854771b75bc4ca8cd542a1d3a666fd80a628d6
Nathan Steed <steedn@outlook.com>
2020-08-11 20:12:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,29 @@
-class Author
+require 'pry'
 
+class Author
   attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
   end
 
   def posts
-    Post.all
+    Post.all.select {|post| post.author == self}
   end
 end
